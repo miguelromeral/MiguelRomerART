bb1fbbc2db1c54e079e8be1b2cae07c87cc094c0
Miguel Romeral <miguel.romeral@gmail.com>
2024-01-05 15:28:07 +0100

    Added MAUI config

--- MR.MAUI/MainPage.xaml.cs
+++ MR.MAUI/MainPage.xaml.cs
@@ -1,13 +1,38 @@
-﻿namespace MR.MAUI
+﻿using Grpc.Net.Client.Balancer;
+using MRA.Services;
+using MRA.Services.Firebase.Interfaces;
+using Plugin.CloudFirestore;
+using System.Diagnostics;
+
+namespace MR.MAUI
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
 
-        public MainPage()
+        DrawingService _drawingService;
+
+        public MainPage(/*IFirestoreService drawingService*/)
         {
             InitializeComponent();
+
+            //var tmp = drawingService;
+
+            _drawingService = Application.Current.MainPage
+                .Handler
+                .MauiContext
+                .Services  // IServiceProvider
+                .GetService<DrawingService>();
+
+            Load();
         }
+
+        public async void Load()
+        {
+            var cloud = await _drawingService.FindDrawingById("cloud");
+            Debug.WriteLine("Cloud: " + cloud?.Name);
+        }
+
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
--- MR.MAUI/MauiProgram.cs
+++ MR.MAUI/MauiProgram.cs
@@ -1,7 +1,38 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Google.Api;
+using Google.Cloud.Firestore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MRA.Services;
+using MRA.Services.AzureStorage;
+using MRA.Services.Firebase;
+using MRA.Services.Firebase.Interfaces;
 
 namespace MR.MAUI
 {
+    public interface IServiceExample
+    {
+        void Hola();
+    }
+
+    public class ServiceExample : IServiceExample
+    {
+        public void Hola()
+        {
+            var tmp = "Hola";
+        }
+    }
+
+    //public class MainViewModel
+    //{
+    //    private readonly IServiceExample _serviceExample;
+
+    //    public MainViewModel(IServiceExample deviceService)
+    //    {
+    //        _serviceExample = deviceService;
+    //    }
+    //}
+
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
@@ -15,11 +46,73 @@ namespace MR.MAUI
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            ConfigureServices(builder);
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
         }
+
+        private static async void ConfigureServices(MauiAppBuilder builder)
+        {
+            var appSettings = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var connectionString = appSettings["AzureStorage:ConnectionString"];
+            var blobStorageContainer = appSettings["AzureStorage:BlobStorageContainer"];
+            var blobURL = appSettings["AzureStorage:BlobPath"];
+
+            var azureStorageService = new AzureStorageService(connectionString, blobStorageContainer, blobURL);
+
+            builder.Services.AddSingleton(azureStorageService);
+
+            var file = @"romerart-6a6c3-firebase-adminsdk-4yop5-839e7a0035.json";
+
+            //new path we will copy the file to. 
+            var newPath = Path.Combine(FileSystem.CacheDirectory, file);
+
+            // read the file in Resource/Raw in this way
+            using var json = await FileSystem.OpenAppPackageFileAsync(file);
+            //create the new path and copy the original json file here
+            using var dest = File.Create(newPath);
+            await json.CopyToAsync(dest);
+
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", newPath);
+            dest.Close();
+
+            //Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @".\Resources\romerart-6a6c3-firebase-adminsdk-4yop5-839e7a0035.json");
+
+            // Configurar la conexión a Firestore
+            var firestoreDbBuilder = new FirestoreDbBuilder { ProjectId = appSettings["Firebase:ProjectID"] };
+            var db = firestoreDbBuilder.Build();
+
+            //var db = FirestoreDb.Create();
+
+            var firebaseService = new FirestoreService(
+                        appSettings["Firebase:CollectionDrawings"],
+                        appSettings["Firebase:CollectionInspirations"],
+                        appSettings["Firebase:CollectionCollections"],
+                        appSettings["AzureStorage:BlobPath"],
+
+                db);
+
+            builder.Services.AddSingleton<IFirestoreService>(firebaseService);
+
+            var drawingService = new DrawingService(azureStorageService, firebaseService);
+
+            builder.Services.AddSingleton(drawingService);
+
+            builder.Services.AddSingleton<IServiceExample, ServiceExample>();
+
+            //// Registra el servicio utilizando un método de fábrica
+            //services.AddSingleton<IDra>(sp =>
+            //{
+            //    return drawingService;
+            //});
+        }
     }
 }
+
