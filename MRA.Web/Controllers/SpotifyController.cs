48ec1096f6945cc7012716058368205132fcb359
Miguel Romeral <miguel.romeral@gmail.com>
2024-01-24 12:52:31 +0100

    Added spotify playlist

---
+++ MRA.Web/Controllers/SpotifyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MRA.Web.Controllers
+{
+    public class SpotifyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Playlist");
+        }
+
+        public IActionResult Playlist()
+        {
+            return View();
+        }
+    }
+}
