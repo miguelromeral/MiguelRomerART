9d7e1e9b7018e98bef2f5c94eeddbb5c0907db6e
Miguel Romeral <devromeral@gmail.com>
2024-07-16 13:40:07 +0200

    Primeros pasos con Web API

---
+++ MRA.WebApi/Controllers/ArtController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRA.Services;
+using MRA.Services.Firebase.Models;
+
+namespace MRA.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArtController : Controller
+    {
+
+        private readonly DrawingService _drawingService;
+        private readonly ILogger<ArtController> _logger;
+
+        public ArtController(ILogger<ArtController> logger, DrawingService drawingService)
+        {
+            _logger = logger;
+            _drawingService = drawingService;
+        }
+
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet("details/{id}")]
+        public async Task<Drawing> Details(string id)
+        {
+            //var model = new DetailsModel(id, SessionSettings.IsLogedAsAdmin(HttpContext.Session.GetString(SessionSettings.USER_ID)));
+            return await _drawingService.FindDrawingById(id, updateViews: true, cache: false);
+        }
+    }
+}
---
+++ MRA.WebApi/Program.cs
@@ -0,0 +1,104 @@
+using Google.Cloud.Firestore;
+using MRA.Services;
+using MRA.Services.Firebase.Interfaces;
+using MRA.Services.Firebase;
+using Microsoft.Extensions.Configuration;
+using MRA.Services.AzureStorage;
+using Google.Apis.Auth.OAuth2;
+using Grpc.Auth;
+using Google.Apis.Storage.v1.Data;
+using Google.Cloud.Firestore.V1;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agrega servicios CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:4200", "https://solomanitas.azurewebsites.net")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var connectionString = builder.Configuration.GetValue<string>("AzureStorage:ConnectionString");
+var blobStorageContainer = builder.Configuration.GetValue<string>("AzureStorage:BlobStorageContainer");
+var blobURL = builder.Configuration.GetValue<string>("AzureStorage:BlobPath");
+
+var azureStorageService = new AzureStorageService(connectionString, blobStorageContainer, blobURL);
+
+builder.Services.AddSingleton(azureStorageService);
+
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @".\Credentials\romerart-6a6c3-firebase-adminsdk-4yop5-839e7a0035.json");
+
+var firebaseService = new FirestoreService(
+            builder.Configuration.GetValue<string>("Firebase:CollectionDrawings"),
+            builder.Configuration.GetValue<string>("Firebase:CollectionInspirations"),
+            builder.Configuration.GetValue<string>("Firebase:CollectionCollections"),
+            builder.Configuration.GetValue<string>("Firebase:CollectionExperience"),
+            builder.Configuration.GetValue<string>("AzureStorage:BlobPath"),
+    FirestoreDb.Create(builder.Configuration.GetValue<string>("Firebase:ProjectID")));
+
+builder.Services.AddSingleton<IFirestoreService>(firebaseService);
+
+var drawingService = new DrawingService(builder.Configuration.GetValue<int>("CacheSeconds"), new MemoryCache(new MemoryCacheOptions()), azureStorageService, firebaseService);
+
+builder.Services.AddSingleton(drawingService);
+
+// Configura el servicio de caché distribuido en memoria
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie("Cookies", options =>
+    {
+        options.Cookie.Name = "CookieMiguelRomeral";
+        options.LoginPath = "/Admin/Login";
+    });
+
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+// Usa el middleware de CORS antes de autenticación y autorización
+app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+var firestoreService = app.Services.GetRequiredService<IFirestoreService>();
+Console.WriteLine($"Firestore Service created: {firestoreService}");
+
+app.MapControllers();
+
+app.Run();
