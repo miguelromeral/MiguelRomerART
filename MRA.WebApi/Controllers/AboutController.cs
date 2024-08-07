b858ca2087d4d53ddabf928f340a1dd8fc1d6db8
Miguel Romeral <devromeral@gmail.com>
2024-08-07 18:54:21 +0200

    Added About Controller

---
+++ MRA.WebApi/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRA.Services;
+using MRA.Services.Firebase.Models;
+
+namespace MRA.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AboutController : Controller
+    {
+
+        private readonly DrawingService _drawingService;
+        private readonly ILogger<AboutController> _logger;
+
+        public AboutController(ILogger<AboutController> logger, DrawingService drawingService)
+        {
+            _logger = logger;
+            _drawingService = drawingService;
+        }
+
+
+        [HttpGet("inspirations")]
+        public async Task<List<Inspiration>> IndexAsync()
+        {
+            return await _drawingService.GetAllInspirations();
+        }
+    }
+}
