1402bda49184d34c7491e1f895c1b5d5e6686052
miguelromeral <miguel.romeral@gmail.com>
2024-10-24 12:08:17 +0200

    Refactor en Program.cs

--- MRA.WebApi/Program.cs
+++ MRA.WebApi/Program.cs
@@ -19,17 +19,19 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//// Configurar logging
-//builder.Logging.ClearProviders();  // Limpia los proveedores de logging preexistentes
-//builder.Logging.AddConsole();      // Agrega logging en consola (visible en Kudu)
-//builder.Logging.AddDebug();        // Agrega logging de depuración
+// Configurar logging
+builder.Logging.ClearProviders();  // Limpia los proveedores de logging preexistentes
+builder.Logging.AddConsole();      // Agrega logging en consola (visible en Kudu)
+builder.Logging.AddDebug();        // Agrega logging de depuración
 
-//// Si necesitas logs de mayor detalle en producción, ajusta el nivel de logging
-//builder.Logging.SetMinimumLevel(LogLevel.Information);  // Cambia a Debug o Trace si necesitas más detalle
+// Si necesitas logs de mayor detalle en producción, ajusta el nivel de logging
+builder.Logging.SetMinimumLevel(LogLevel.Information);  // Cambia a Debug o Trace si necesitas más detalle
 
+var logger = builder.Logging.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>(); // Obtener el logger
+logger.LogInformation("Iniciando la aplicación...");
 
-// Agregar JWT
-
+// Añadimos Autenticación por JWT
+logger.LogInformation("Iniciando la configuración de JWT");
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
 
@@ -53,12 +55,13 @@ builder.Services.AddAuthentication(options =>
 });
 
 // Agrega servicios CORS
+logger.LogInformation("Agregando servicios CORS");
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
     builder =>
     {
-        builder.WithOrigins("http://localhost:4200", "https://solomanitas.azurewebsites.net")
+        builder.WithOrigins("http://localhost:4200", "https://miguelromeral.azurewebsites.net/")
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
@@ -66,45 +69,49 @@ builder.Services.AddCors(options =>
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+// Configuración de Azure
+logger.LogInformation("Configurando conexión con Azure Storage");
 var connectionString = builder.Configuration.GetValue<string>("AzureStorage:ConnectionString");
 var blobStorageContainer = builder.Configuration.GetValue<string>("AzureStorage:BlobStorageContainer");
 var blobURL = builder.Configuration.GetValue<string>("AzureStorage:BlobPath");
 
 var azureStorageService = new AzureStorageService(connectionString, blobStorageContainer, blobURL);
-
 builder.Services.AddSingleton(azureStorageService);
 
+// Configuración de Credenciales de Firebase
+logger.LogInformation("Configurando credenciales de Google Firebase");
 var serviceAccountPath = "";
-//#if DEBUG
-//serviceAccountPath = @".\Credentials\romerart-6a6c3-firebase-adminsdk-4yop5-839e7a0035.json";
-//Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", serviceAccountPath);
-//#endif
 
 // Si estás en Azure, crea el archivo temporal desde la variable de entorno
 var googleCredentialsJson = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON");
 if (!string.IsNullOrEmpty(googleCredentialsJson))
 {
+    logger.LogInformation("Detectada variable de entorno para las credenciales. Creando fichero temporal");
     var tempCredentialPath = Path.Combine(Path.GetTempPath(), "firebase-credentials.json");
     File.WriteAllText(tempCredentialPath, googleCredentialsJson);
 
     // Establecer la variable de entorno GOOGLE_APPLICATION_CREDENTIALS para que apunte al archivo temporal
-    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", tempCredentialPath);
     serviceAccountPath = tempCredentialPath;
 }
 else
 {
     // Si estoy en local
+    logger.LogInformation("Obteniendo credenciales desde fichero almacenado en el sistema de archivos");
     serviceAccountPath = @".\Credentials\romerart-6a6c3-firebase-adminsdk-4yop5-839e7a0035.json";
-    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", serviceAccountPath);
 }
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", serviceAccountPath);
 
+// Configuración de Firebase
+logger.LogInformation("Configurando credenciales de Google Firebase");
 var firebaseProjectId = builder.Configuration.GetValue<string>("Firebase:ProjectID");
 var secondsCache = builder.Configuration.GetValue<int>("CacheSeconds");
 
 //var accessToken = await GoogleCredentialHelper.GetAccessTokenAsync(serviceAccountPath);
+logger.LogInformation("Creando servicio de Remote Config");
 var remoteConfigService = new RemoteConfigService(new MemoryCache(new MemoryCacheOptions()), firebaseProjectId, serviceAccountPath, secondsCache);
 builder.Services.AddSingleton(remoteConfigService);
 
+logger.LogInformation("Creando servicio de Firebase");
 var firebaseService = new FirestoreService(
             builder.Configuration.GetValue<string>("Firebase:CollectionDrawings"),
             builder.Configuration.GetValue<string>("Firebase:CollectionInspirations"),
@@ -126,6 +133,7 @@ builder.Services.AddSession();
 
 // Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -137,22 +145,23 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.LoginPath = "/Admin/Login";
     });
 
+// Añadir Memoria Cache
 builder.Services.AddMemoryCache();
 
+logger.LogInformation("Creando aplicación");
 var app = builder.Build();
 
-// JWT
-
+// Utilizando autenticación JWT
 app.UseAuthentication();
 app.UseAuthorization();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -167,9 +176,8 @@ app.UseCors("AllowSpecificOrigin");
 app.UseAuthentication();
 app.UseAuthorization();
 
-var firestoreService = app.Services.GetRequiredService<IFirestoreService>();
-Console.WriteLine($"Firestore Service created: {firestoreService}");
-
 app.MapControllers();
+
+logger.LogInformation("Aplicación creada con éxito. Procede a ejecutarse");
 
 app.Run();
