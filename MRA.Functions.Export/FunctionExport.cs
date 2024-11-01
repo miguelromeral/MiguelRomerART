80d3f4d5f7f5a69089136bec5abc00f907acda99
miguelromeral <miguel.romeral@gmail.com>
2024-11-01 12:38:12 +0100

    Added config in loca.settings

--- MRA.Functions.Export/FunctionExport.cs
+++ MRA.Functions.Export/FunctionExport.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace MRA.Functions.Export
@@ -8,16 +9,32 @@ namespace MRA.Functions.Export
     public class FunctionExport
     {
         private readonly ILogger<FunctionExport> _logger;
+        private readonly IConfiguration _configuration;
 
-        public FunctionExport(ILogger<FunctionExport> logger)
+        public FunctionExport(ILogger<FunctionExport> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
+
         [FunctionName("FunctionExport")]
-        public void Run([TimerTrigger("*/15 * * * * *")]TimerInfo myTimer)
+        public void Run([TimerTrigger("*/10 * * * * *")] TimerInfo myTimer)
         {
-            _logger.LogInformation($"EJECUCIÓN DE FUNCIÓN {DateTime.Now}");
+            try
+            {
+                _logger.LogInformation($"EJECUCIÓN DE FUNCIÓN {DateTime.Now}");
+
+                //var firestoreService = new FirestoreService(_configuration);
+                //_logger.LogWarning("Proyecto: "+firestoreService.ProjectId);
+                
+                _logger.LogWarning("Proyecto: " + _configuration["Firebase:ProjectID"]);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error en la ejecución de la función: {ex.Message}");
+            }
         }
     }
 }
