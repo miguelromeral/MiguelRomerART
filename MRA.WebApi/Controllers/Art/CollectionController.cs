40199c00b1216b8bea7c455634e1f1c8d0accaf4
miguelromeral <miguel.romeral@gmail.com>
2025-04-15 16:02:13 +0200

    Splitted Art Controller

---
+++ MRA.WebApi/Controllers/Art/CollectionController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRA.Services.Models.Collections;
+using MRA.Services.Models.Drawings;
+using MRA.Services;
+using Microsoft.AspNetCore.Authorization;
+using MRA.DTO.Exceptions;
+using MRA.WebApi.Models.Responses;
+using MRA.DTO.Models;
+using MRA.WebApi.Models.Requests;
+
+namespace MRA.WebApi.Controllers.Art;
+
+[ApiController]
+[Route("api/art/collection")]
+public class CollectionController : Controller
+{
+    private readonly IAppService _appService;
+    private readonly ICollectionService _collectionService;
+    private readonly ILogger<CollectionController> _logger;
+
+    public CollectionController(
+        ILogger<CollectionController> logger,
+        IAppService appService,
+        ICollectionService collectionService
+        )
+    {
+        _logger = logger;
+        _appService = appService;
+        _collectionService = collectionService;
+    }
+
+
+    [HttpGet("details/{id}")]
+    public async Task<ActionResult<CollectionResponse>> Details(string id)
+    {
+        return await FetchCollectionDetails(id, true);
+    }
+
+
+    [Authorize]
+    [HttpGet("full-details/{id}")]
+    public async Task<ActionResult<CollectionResponse>> FullDetails(string id)
+    {
+        return await FetchCollectionDetails(id, false);
+    }
+
+    private async Task<ActionResult<CollectionResponse>> FetchCollectionDetails(string id, bool onlyIfVisible)
+    {
+        try
+        {
+            _logger.LogInformation($"Solicitados detalles públicos de colección \"{id}\"");
+            var collection = await _appService.FindCollectionByIdAsync(id, onlyIfVisible: onlyIfVisible, cache: true);
+            return Ok(new CollectionResponse(collection));
+        }
+        catch (CollectionNotFoundException cnf)
+        {
+            _logger.LogWarning($"No se encontró ninguna colección \"{id}\"");
+            return NotFound(new { message = $"No collection found with ID \"{id}\"" });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error al recuperar los detalles públicos de la colección \"{id}\": " + ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new { message = $"Error when retrieving collection with ID \"{id}\"" });
+        }
+    }
+
+    [HttpGet("exist/{id}")]
+    public async Task<ActionResult<bool>> Exists(string id)
+    {
+        try
+        {
+            _logger.LogInformation($"Comprobando si existe colección \"{id}\"");
+            var exists = await _collectionService.ExistsCollection(id);
+            _logger.LogInformation($"Existe colección \"{id}\": {(exists ? "Sí" : "No")}");
+            return Ok(exists);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error al comprobar colección \"{id}\": " + ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new { message = $"Error when checking collection \"{id}\"" });
+        }
+    }
+
+    [Authorize]
+    [HttpPost("save/{id}")]
+    public async Task<ActionResult<CollectionResponse>> Save(string id, [FromBody] SaveCollectionRequest model)
+    {
+        try
+        {
+            _logger.LogInformation($"Guardando colección \"{model.Id}\"");
+            var collection = new CollectionModel()
+            {
+                Id = model.Id,
+                Description = model.Description,
+                Name = model.Name,
+                Order = model.Order,
+                DrawingIds = model.DrawingsIds
+            };
+            if (String.IsNullOrEmpty(collection.Id))
+            {
+                _logger.LogWarning("No se ha proporcionado un ID correcto para la colección");
+                return BadRequest(new { message = "No correct ID provided for the collection" });
+            }
+
+            await _collectionService.SaveCollectionAsync(id, collection);
+            _logger.LogInformation($"Guardada colección \"{model.Id}\" con éxito");
+            _appService.CleanAllCache();
+            return new CollectionResponse(collection);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error al guardar colección \"{id}\": " + ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new { message = $"Error when saving collection \"{id}\"" });
+        }
+    }
+
+    [Authorize]
+    [HttpPost("delete")]
+    public async Task<ActionResult<bool>> Delete([FromBody] string id)
+    {
+        try
+        {
+            _logger.LogInformation($"Eliminando colección \"{id}\"");
+            await _collectionService.DeleteCollection(id);
+            _logger.LogInformation($"Colección \"{id}\" eliminada con éxito");
+            _appService.CleanAllCache();
+            return Ok(true);
+        }
+        catch (CollectionNotFoundException cnf)
+        {
+            _logger.LogWarning($"No se encontró ninguna colección \"{id}\"");
+            return NotFound(new { message = $"No collection found with ID \"{id}\"" });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error al eliminar colección \"{id}\": " + ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                new { message = $"Error when removing collection \"{id}\"" });
+        }
+    }
+}
